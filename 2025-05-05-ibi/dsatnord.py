58b11a8dd22dfe647539e80f9dfface49eb4e61e
Robert Jäschke <robert.jaeschke@hu-berlin.de>
2025-04-18 19:54:04 +0200

    colors fixed

--- 2025-05-05-ibi/dsatnord.py
+++ 2025-05-05-ibi/dsatnord.py
@@ -16,43 +16,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 parts = [
-    (2, 16, "header"),
-    (3, 20 * 4 + 169 * 4 + 2280 + 30*4 + 844 + 2990*4 + 41245 * 4 + 135064, "offsets"),
-    (5, 754077, "tiles0"),
-    (6, 9056940, "tiles1"),
-    (10, 567760, "paths"),
-    (11, 975894 + 994802, "topware"),
-    (1, 13394 * 64, "places"),
-    (12, 24 * 16, "unknown"),
-    (9, 4878 * 201, "citysigns"),
-    (4, 1691200, "paths"),
-    (7, 70627806, "tiles2"),
-    (8, 644804703 + 936 - 86822577, "tiles3"),
-    (12, 644824916 - 644804703 - 936, "unknown"),
-    (12, 8995, "unknown")
+    (2, "red", 16, "header"),
+    (3, "blue", 20 * 4 + 169 * 4 + 2280 + 30*4 + 844 + 2990*4 + 41245 * 4 + 135064, "offsets"),
+    (5, "#267d40", 754077, "tiles0"),
+    (6, "#31a354", 9056940, "tiles1"),
+    (10, "#3182bd", 567760, "paths"),
+    (11, "#fd8d3c", 975894 + 994802, "topware"),
+    (1, "#974ea3", 13394 * 64, "places"),
+    (12, "#666666", 24 * 16, "unknown"),
+    (9, "#e6550d", 4878 * 201, "citysigns"),
+    (4, "#6baed6", 1691200, "paths"),
+    (7, "#73c475", 70627806, "tiles2"),
+    (8, "#a1d99b", 644804703 + 936 - 86822577, "tiles3"),
+    (12, "#666666", 644824916 - 644804703 - 936, "unknown"),
+    (12, "#666666", 8995, "unknown")
 ]
 
 fname = "images/dsatnord"
 
 if __name__ == '__main__':
-    sizes = np.array([size for pos, size, _ in parts])
+    sizes = np.array([size for pos, color, size, _ in parts])
     total_size = sum(sizes)
-    colors = plt.colormaps['tab20'](np.linspace(0.15, 0.85, len(sizes)))
 
     done = 0
-    for i in range(max([pos for pos, size, _ in parts]) + 2):
+    for i in range(max([pos for pos, color, size, _ in parts]) + 2):
         fig, ax = plt.subplots(figsize=(15.15, 1))
         ax.xaxis.set_visible(False)
         ax.yaxis.set_visible(False)
-        # ax.set_frame_on(False)
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(False)
-        # ax.spines['bottom'].set_visible(False)
-        ax.spines['left'].set_visible(False)
+        for d in ['top', 'right', 'left']:
+            ax.spines[d].set_visible(False)
         ax.set_xlim(0, sizes.sum())
 
         size_cum = 0
-        for (pos, size, name), color in zip(parts, colors):
+        for pos, color, size, name in parts:
             c = color if pos <= i else "white" # switch parts (in)visible
             b = "black" if pos == i else c
             if pos == i:
